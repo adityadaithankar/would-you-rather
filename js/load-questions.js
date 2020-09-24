c5d345bbbb99fa6eeedf4f5de5c61819d5a8dbb9
Aditya Daithankar <github.com/adityadaithankar>
2020-09-24 21:07:17 +0530

    multi select checkbox to select types of questions to load

--- js/load-questions.js
+++ js/load-questions.js
@@ -1,27 +1,10 @@
-let questions = [
-  "Hello",
-  "How",
-  "You",
-  "Yes",
-  "Me"
-];
-
-function displayQuestion() {
-  console.log(questions);
-  if (questions.length > 0) {
-    var random = Math.floor(Math.random() * questions.length);
-    document.getElementById("question").innerHTML = questions[random];
-
-    console.log(questions[random]);
-
-    // Update array to remove the question that has been displayed
-    if (random > -1) {
-      questions.splice(random, 1);
+function loadQuestions() {
+  let selectedQuestionTypes = [];
+  let questionTypes = document.getElementsByName('questionType');
+  for (i = 0; i < questionTypes.length; i++) {
+    if (questionTypes[i].checked) {
+      selectedQuestionTypes.push(questionTypes[i].value);
     }
-
-    console.log(questions);
-  } else { // If all questions from the array have been displayed, show end of game message
-    document.getElementById("question").innerHTML = "END OF GAME. Reload page";
-    // Function to reload page -> location.reload();
   }
+  console.log(selectedQuestionTypes);
 }
