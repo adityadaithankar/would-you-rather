841cc3f763a0479125665178080581b3233deec6
Aditya Daithankar <github.com/adityadaithankar>
2020-09-25 17:55:07 +0530

    condensed sentence structuring for a naughty and a safe question

--- js/questions/naughty.js
+++ js/questions/naughty.js
@@ -1,4 +1,4 @@
 let naughty = [
   "sleep with your best friend's lover or your lover's best friend",
-  "sleep with the person on your left or on your right"
+  "sleep with the person on your left or right"
 ];
--- js/questions/safe.js
+++ js/questions/safe.js
@@ -1,3 +1,3 @@
 let safe = [
-  "be the poorest kid in the world or the richest oldest person in the world"
+  "be the poorest kid or the richest oldest person in the world"
 ];
