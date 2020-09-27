4f7e6d2508a553f34e04e4d81dac7805b7bcd795
Aditya Daithankar <github.com/adityadaithankar>
2020-09-27 20:45:20 +0530

    added safe and naughty questions to now make 3 questions across all categories

--- js/questions/naughty.js
+++ js/questions/naughty.js
@@ -1,4 +1,5 @@
 let naughty = [
   "sleep with your best friend's lover or your lover's best friend",
-  "sleep with the person on your left or right"
+  "sleep with the person on your left or right",
+  "date a new person every month or only one person throughout your life"
 ];
--- js/questions/safe.js
+++ js/questions/safe.js
@@ -1,3 +1,5 @@
 let safe = [
-  "be the poorest kid or the richest oldest person in the world"
+  "be the poorest kid or the richest oldest person in the world",
+  "have the power of flight or the power of invisibility",
+  "have money or fame"
 ];
